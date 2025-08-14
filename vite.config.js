459252666f5c0098f7ca22b583bcd5d10e159681
duglco <bredshall@gmail.com>
2025-08-14 10:00:09 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-const repo = 'mymilpcs'; // <- your repo
-
-export default defineConfig({
-  plugins: [react()],
-  base: `/${repo}/`,
-})
